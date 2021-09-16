5bf86262aa9c6591e7b3e0a87805540e7b1f2706
nickobrad <nicholas.omondi@student.moringaschool.com>
2021-09-17 01:40:28 +0300

    signals.py created

---
+++ review_app/signals.py
@@ -0,0 +1,11 @@
+from django.db.models.signals import post_save
+from django.dispatch import receiver
+from .models import Profile
+from django.contrib.auth import get_user_model
+
+User = get_user_model()
+
+@receiver(post_save, sender = User)
+def post_save_create_profile(sender, instance, created, **kwargs):
+    if created:
+        Profile.objects.create(user = instance)
