ff54a18b2dddf1aa3aa4e8f3dbe47aa06db82723
nickobrad <nicholas.omondi@student.moringaschool.com>
2021-09-20 23:40:35 +0300

    Edits in the urls

--- review_app/urls.py
+++ review_app/urls.py
@@ -4,12 +4,24 @@ from django.urls.conf import include
 from django.conf.urls import url
 from django.conf import settings
 from django.conf.urls.static import static
-from .views import loginuser, register, logout_view, search_results
+from .views import OneProfile, OneProject, loginuser, my_profile, project, rate, register, logout_view, search_results, HomeView, HomeView2, UpdateProjectView, UpdateProfileView, ProjectList, ProfileList
+from rest_framework.authtoken.views import obtain_auth_token
 
 urlpatterns = [
     path('', loginuser, name = 'login' ),
-    path('register/', register, name = 'register'),  
-    url(r'^search/', search_results, name = 'search_users'),
+    path('register/', register, name = 'register'), 
+    path('home/', HomeView, name = 'home'), 
+    path('project/<int:pk>', project, name = 'project'), 
+    path('project/review/<int:pk>', rate, name = 'rate'), 
+    path('profile', my_profile, name = 'profile'),
+    path('update/project/<int:pk>', UpdateProjectView.as_view(), name = 'update_project'),
+    path('update/profile/<int:pk>', UpdateProfileView.as_view(), name = 'update_profile'),
+    path('search/', search_results, name = 'search_users'),
+    path('reviews/api/projects/', ProjectList.as_view()),
+    path('reviews/api/profiles/', ProfileList.as_view()),
+    path('api-token-auth/', obtain_auth_token),
+    path('api/project/<int:pk>/',OneProject.as_view()),
+    path('api/profile/<int:pk>/',OneProfile.as_view()),
     path('logout', logout_view, name='logout'), 
 ]
 if settings.DEBUG:
