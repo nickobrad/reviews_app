e3bccca84e9f53d3faa08d261a26f01aceb7750c
nickobrad <nicholas.omondi@student.moringaschool.com>
2021-09-20 23:39:26 +0300

    Register models in admin

--- review_app/admin.py
+++ review_app/admin.py
@@ -1,3 +1,8 @@
 from django.contrib import admin
+from .models import Projects, Profile, Review
+from django.contrib.auth.models import User
 
 # Register your models here.
+admin.site.register(Profile)
+admin.site.register(Projects)
+admin.site.register(Review)
